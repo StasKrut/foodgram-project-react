356b9d113e2173a8ef1077ee813dd2566256a459
Станислав Крутских <ahtu14@gmail.com>
2022-12-29 23:07:47 +0300

    Поправлен фильтр рецептов по списку покупок

--- backend/api/v1/utils.py
+++ backend/api/v1/utils.py
@@ -19,11 +19,12 @@ def download_shopping_cart(request):
             else:
                 shopping_list[name]['amount'] += amount
     content = (
+        'Список покупок:\n',
         [f'{item["name"]} ({item["measurement_unit"]}) '
          f'- {item["amount"]}\n'
          for item in shopping_list.values()]
     )
-    filename = 'shopping_list.pdf'
+    filename = 'shopping_list.txt'
     response = HttpResponse(content, content_type='text/plain')
     response['Content-Disposition'] = (
         'attachment; filename={0}'.format(filename)
