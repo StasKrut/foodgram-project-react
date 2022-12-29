7bd46a221e316c9db75b15adacc9b45709caeb73
Станислав Крутских <ahtu14@gmail.com>
2022-12-29 22:35:23 +0300

    Поправлен фильтр рецептов по списку покупок

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -23,5 +23,5 @@ class RecipeFilter(filters.FilterSet):
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
