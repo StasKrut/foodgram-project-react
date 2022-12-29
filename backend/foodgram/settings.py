adc478acd3f715ee7d76468d5ee6a6b4b50e525b
Станислав Крутских <ahtu14@gmail.com>
2022-12-29 20:14:58 +0300

    Поправлена валидация времени готовки, поправлен nginx.conf

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'colorfield',
     'rest_framework',
     'djoser',
@@ -38,7 +39,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ['*']
 
 ROOT_URLCONF = 'foodgram.urls'
 
